42f80d4d3a5808e1c9c7a30cbaabaef37739e396
Lovac42 <lovac42@protonmail.com>
2019-01-24 00:19:10 -0500

    added screens, updated copyright

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 AnkiAddon: Randomize New Queue
 
 
+## About:
 Similar to Hoochie Mama, but uses a custom queue builder and operates on the new card queue. This addon must be enabled in preferences. And you can also enable "Mix new cards and reviews" as well as Hoochie Mama for the full on hoochieness...
+
+V1 & V2 compatible
+
+## Config:
+This addon must be enabled in preferences menu under the "Muffins" tab.
+
+<img src="https://github.com/lovac42/HoochiePapa/blob/master/screenshots/prefmenu.png?raw=true">
--- src/hoochiePapa.py
+++ src/hoochiePapa.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright: (C) 2018 Lovac42
+# Copyright: (C) 2018-2019 Lovac42
 # Support: https://github.com/lovac42/HoochiePapa
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.9
+# Version: 0.1.0
 
 
 # == User Config =========================================
@@ -144,7 +144,7 @@ def setupUi(self, Preferences):
 
     r=self.lrnStageGLayout.rowCount()
     self.hoochiePapa = QtWidgets.QCheckBox(self.lrnStage)
-    self.hoochiePapa.setText(_('Hoochie Papa! Randomize NEW'))
+    self.hoochiePapa.setText(_('Hoochie Papa! Randomize New'))
     self.lrnStageGLayout.addWidget(self.hoochiePapa, r, 0, 1, 3)
 
 
