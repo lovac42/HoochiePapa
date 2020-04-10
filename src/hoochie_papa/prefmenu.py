9e175b8516391dece12d65ec4ad78d877298bc22
lovac42 <lovac42@protonmail.com>
2020-04-10 18:49:28 -0400

    refactored code to use getMuffinsGroupbox from lib

--- src/hoochie_papa/prefmenu.py
+++ src/hoochie_papa/prefmenu.py
@@ -10,28 +10,17 @@ from aqt.qt import *
 from anki.lang import _
 from anki.hooks import wrap
 
-
 from .sort import CUSTOM_SORT
-from .lib.com.lovac42.anki.version import ANKI21, ANKI20
+from .lib.com.lovac42.anki.version import ANKI20
 from .lib.com.lovac42.anki.gui.checkbox import TristateCheckbox
 from .lib.com.lovac42.anki.gui import muffins
 
 
-if ANKI21:
-    from PyQt5 import QtCore, QtGui, QtWidgets
-else:
-    from PyQt4 import QtCore, QtGui as QtWidgets
-
-
 def setupUi(self, Preferences):
-    grid_layout = muffins.getMuffinsTab(self)
-    r = grid_layout.rowCount()
+    papa_groupbox = muffins.getMuffinsGroupbox(self, "Hoochie Papa!")
+    papa_grid_layout = QGridLayout(papa_groupbox)
 
-    papa_groupbox = QtWidgets.QGroupBox(self.lrnStage)
-    papa_groupbox.setTitle("Hoochie Papa!")
-    papa_grid_layout = QtWidgets.QGridLayout(papa_groupbox)
-    grid_layout.addWidget(papa_groupbox, r, 0, 1, 3)
-
+    r=0
     self.hoochiePapa = TristateCheckbox(papa_groupbox)
     self.hoochiePapa.setTristate(False)
     self.hoochiePapa.setDescriptions({
@@ -39,16 +28,15 @@ def setupUi(self, Preferences):
         Qt.PartiallyChecked: "Error, should not have seen this.",
         Qt.Checked:          "Randomize new cards, check subdeck limits",
     })
-
     papa_grid_layout.addWidget(self.hoochiePapa, r, 0, 1, 3)
     self.hoochiePapa.clicked.connect(lambda:toggle(self))
 
     r+=1
-    self.hoochiePapaSortLbl=QtWidgets.QLabel(papa_groupbox)
+    self.hoochiePapaSortLbl=QLabel(papa_groupbox)
     self.hoochiePapaSortLbl.setText(_("      Sort new cards by:"))
     papa_grid_layout.addWidget(self.hoochiePapaSortLbl, r, 0, 1, 1)
 
-    self.hoochiePapaSort = QtWidgets.QComboBox(papa_groupbox)
+    self.hoochiePapaSort = QComboBox(papa_groupbox)
     sort_itms = CUSTOM_SORT.iteritems if ANKI20 else CUSTOM_SORT.items
     for i,v in sort_itms():
         self.hoochiePapaSort.addItem("")
@@ -56,7 +44,7 @@ def setupUi(self, Preferences):
     papa_grid_layout.addWidget(self.hoochiePapaSort, r, 1, 1, 3)
 
     r+=1
-    footnote_label = QtWidgets.QLabel(papa_groupbox)
+    footnote_label = QLabel(papa_groupbox)
     footnote_label.setText(_("&nbsp;&nbsp;&nbsp;<i>* If there is anything you don't understand, GIYF.</i>"))
     papa_grid_layout.addWidget(footnote_label, r, 0, 1, 3)
 
